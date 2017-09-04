173fbaf821c0c003dfafb5b4f34d383e3e58c30d
Mohinee Toshniwal <mohineet@gmail.com>
2017-09-05 00:03:03 +0530

    modal is fixed

--- books.js
+++ books.js
@@ -1,28 +1,29 @@
 function myBookFunction(id){
-    var modal = document.getElementById('myModal');
-    modal.style.display = "block";
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+  var modal = document.getElementById('myModal');
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
       modal.style.display = "none";
-    }  
+  }
+  modal.style.display = "block";
   var save = document.getElementById("save");
-  var myPhoneno = document.getElementById('password_textbox').value;
-  console.log(" phone= "+myPhoneno);
-  event.preventDefault();
-  $.ajax({
-    url: "searchForDb.php?phone="+myPhoneno,
-    method: 'GET',
-    type: 'json',
-    success: function(data){
-      if(data == 'success'){
-        window.location.href = 'https://drive.google.com/open?id='+id;
-      }else{
-        getPrompt(myPhoneno, id);
+  save.onclick = function() {
+    modal.style.display = "none";
+    var myPhoneno = document.getElementById('password_textbox').value;
+    console.log(" phone= "+myPhoneno);
+    event.preventDefault();
+    $.ajax({
+      url: "searchForDb.php?phone="+myPhoneno,
+      method: 'GET',
+      type: 'json',
+      success: function(data){
+        if(data == 'success'){
+          window.location.href = 'https://drive.google.com/open?id='+id;
+        }else{
+          getPrompt(myPhoneno, id);
+        }
       }
-    }
-  }); 
+    }); 
+  }
 }
 
 
@@ -40,6 +41,7 @@ function getPrompt(myPhoneno, id){
       if(data == 'success'){
         window.location.href = 'https://drive.google.com/open?id='+id;
     }else{
+        alert("Something went wrong. Please try again!");
         console.log("Something went wrong. Please try again!");
     }
   }
