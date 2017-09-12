37a5d4f29d7d1bb011e390ae68e5e81731b22da5
preethiravi14 <preethiravi14@gmail.com>
2017-09-12 19:36:58 +0530

    'all'

--- books.js
+++ books.js
@@ -1,48 +1,61 @@
+﻿
 function myBookFunction(id){
-    var modal = document.getElementById('myModal');
-    modal.style.display = "block";
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
+  var modal = document.getElementById('myModal');
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
       modal.style.display = "none";
-    }  
+  }
+  modal.style.display = "block";
   var save = document.getElementById("save");
-  var myPhoneno = document.getElementById('password_textbox').value;
-  console.log(" phone= "+myPhoneno);
-  event.preventDefault();
-  $.ajax({
-    url: "searchForDb.php?phone="+myPhoneno,
-    method: 'GET',
-    type: 'json',
-    success: function(data){
-      if(data == 'success'){
-        window.location.href = 'https://drive.google.com/open?id='+id;
-      }else{
-        getPrompt(myPhoneno, id);
+  save.onclick = function() {
+    modal.style.display = "none";
+    var myPhoneno = document.getElementById('password_textbox').value;
+    console.log(" phone= "+myPhoneno);
+    event.preventDefault();
+    $.ajax({
+      url: "searchForDb.php?phone="+myPhoneno,
+      method: 'GET',
+      type: 'json',
+      success: function(data){
+        if(data == 'success'){
+          window.location.href = 'https://drive.google.com/open?id='+id;
+        }else{
+          getPrompt(myPhoneno, id);
+        }
       }
-    }
-  }); 
+    }); 
+  }
 }
 
 
 function getPrompt(myPhoneno, id){
-  var otp = prompt("otp Here:","");
-  var myName = prompt("Name Here:","");
-  var myEmail = prompt("Email Here:","");
-  var myLocation = prompt("Location Here:",""); 
-  $.ajax({
-    url: "saveToDb.php?email="+myEmail+"&name="+myName+"&location="+myLocation+"&phone="+myPhoneno+"&otp="+otp,
-    method: 'GET',
-    type: 'json',
-    success: function(data){
-      console.log(data);
-      if(data == 'success'){
-        window.location.href = 'https://drive.google.com/open?id='+id;
-    }else{
-        console.log("Something went wrong. Please try again!");
-    }
+  var modal = document.getElementById('detailsModal');
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+      modal.style.display = "none";
   }
-});
+  modal.style.display = "block";
+  var save = document.getElementById("saveDetails");
+  save.onclick = function() {
+    modal.style.display = "none";
+    var myName = document.getElementById('name').value;
+    var myLocation = document.getElementById('location').value;
+    var myEmail = document.getElementById('email').value;
+    var otp = document.getElementById('otp').value;
+    $.ajax({
+      url: "saveToDb.php?email="+myEmail+"&name="+myName+"&location="+myLocation+"&phone="+myPhoneno+"&otp="+otp,
+      method: 'GET',
+      type: 'json',
+      success: function(data){
+        console.log(data);
+        if(data == 'success'){
+          window.location.href = 'https://drive.google.com/open?id='+id;
+        }else{
+          alert("Something went wrong. Please try again!");
+          console.log("Something went wrong. Please try again!");
+        }
+      }
+    });
+  }
 }
 
